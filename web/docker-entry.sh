448d521cf6dbeb9edf044203a9a6ddc650bac2bd
Yi EungJun <eungjun.yi@navercorp.com>
2016-06-02 01:36:36 +0900

    wip

--- web/docker-entry.sh
+++ web/docker-entry.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo Starting Nginx
 sed -Ei "s/PLATFORM_PORT/$PLATFORM_PORT_80_TCP_PORT/" /etc/nginx/sites-available/mattermost
+if [ "$MATTERMOST_ENABLE_SSL" = true ]; then
+    ssl="-ssl"
+fi
+ln -s /etc/nginx/sites-available/mattermost$ssl /etc/nginx/sites-enabled/mattermost
 nginx -g 'daemon off;'
